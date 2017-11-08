b694dea61ee87fdc07ab425899213e9546030e39
Jesús Bueno Urbano <33157569+buenourbano@users.noreply.github.com>
2017-11-08 12:25:32 +0100

    Update ejercicio2.py

--- sm/ejercicio2.py
+++ sm/ejercicio2.py
@@ -1,10 +1,11 @@
 #ENUNCIADO
 #Rediseñe la anterior función para que proporcione un generador, caso de no incluir ya esa funcionalidad.
 
-def floatingrange2(comienzo,final,paso):
-    elemento = float(comienzo)
-    parada = float(final)
-    pasolocal = float(paso)
-    while elemento < parada:
-        yield elemento
-        elemento += pasolocal
+def floatingrange2(comienzo,final,paso=0.1):
+    elemento = comienzo
+    if paso==0:
+        paso=0.1
+    else:
+        while elemento < final:
+            yield elemento
+            elemento += paso
